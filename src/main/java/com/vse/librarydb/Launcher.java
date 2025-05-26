807f0688da6f203dd10b329261d91d535d0a0276
Wasabbyy <f.stupar@seznam.cz>
2025-05-26 18:15:10 +0200

    Adding fat jar to the project

---
+++ src/main/java/com/vse/librarydb/Launcher.java
@@ -0,0 +1,7 @@
+package com.vse.librarydb;
+
+public class Launcher {
+    public static void main(String[] args) {
+        LibraryApp.main(args); // Delegates to your existing Application class
+    }
+}
